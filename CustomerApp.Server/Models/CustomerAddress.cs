838ae4ab83a647c57c22d0a5996681fa8059f517
Clarissa Lopez <102737214+lopezclarissa2021@users.noreply.github.com>
2024-02-06 17:43:32 -0800

    Add key to model

--- CustomerApp.Server/Models/CustomerAddress.cs
+++ CustomerApp.Server/Models/CustomerAddress.cs
@@ -1,7 +1,10 @@
-﻿namespace CustomerApp.Server.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerApp.Server.Models
 {
     public class CustomerAddress
     {
+        [Key]
         public int Id { get; set; }
         public int CustomerId { get; set; }
         public int AddressId { get; set; }
